e5b43cbc0afd21ea40de67c0ff065861df8b54c4
max-powell <github email address>
2019-09-04 16:11:25 +0100

    added /users route

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+require('./db/mongoose')
+
+const User = require('./models/User')
 
 const app = express ()
 const port = process.env.PORT || 3000
@@ -6,8 +9,12 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 
 app.post('/users', (req, res) => {
-  console.log(req.body)
-  res.send('testing')
+  const user = new User(req.body)
+  user.save()
+  .then(() => res.send(user))
+  .catch((e) => {
+    res.status(400).send(e)
+  })
 })
 
 app.listen(port, () => {
