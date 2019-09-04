573af1df6e9797682ece95ff73cee74263d84d72
max-powell <github email address>
2019-09-04 16:28:17 +0100

    added user index route

--- src/index.js
+++ src/index.js
@@ -18,6 +18,12 @@ app.post('/users', (req, res) => {
   })
 })
 
+app.get('/users', (req, res) => {
+  User.find({})
+  .then(users => res.send(users))
+  .catch(e => res.status(500).send())
+})
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body)
   task.save()
