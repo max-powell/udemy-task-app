bb43ef21febbed6f9cb9ca6dec164a8d376ba288
max-powell <github email address>
2019-09-04 17:11:19 +0100

    added read task route

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ app.post('/users', (req, res) => {
   const user = new User(req.body)
   user.save()
   .then(() => res.status(201).send(user))
-  .catch((e) => res.status(400).send(e))
+  .catch(e => res.status(400).send(e))
 })
 
 app.get('/users', (req, res) => {
   User.find({})
   .then(users => res.send(users))
-  .catch(e => res.status(500).send())
+  .catch(() => res.status(500).send())
 })
 
 app.get('/users/:id', (req, res) => {
@@ -32,20 +32,34 @@ app.get('/users/:id', (req, res) => {
 
     res.send(user)
   })
-  .catch()
+  .catch(e => res.status(500).send(e))
 })
 
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body)
   task.save()
   .then(() => res.status(201).send(task))
-  .catch((e) => res.status(400).send(e))
+  .catch(e => res.status(400).send(e))
 })
 
 app.get('/tasks', (req, res) => {
   Task.find({})
   .then(tasks => res.send(tasks))
   .catch(() => res.status(500).send())
+})
+
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id
+  Task.findById(_id)
+  .then(task => {
+    if (!task) {
+      return res.status(404).send()
+    }
+    res.send(task)
+  })
+  .catch(e => {
+    res.status(500).send(e)
+  })
 })
 
 app.listen(port, () => {
