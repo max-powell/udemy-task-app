e10da1e10d6fdd566d17fd60e5d256eee3d9beeb
max-powell <github email address>
2019-09-05 13:26:43 +0100

    added login route and static findByCredentials method for User

--- src/models/user.js
+++ src/models/user.js
@@ -21,6 +21,7 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
     trim: true,
     lowercase: true,
     validate: {
@@ -46,8 +47,24 @@ const userSchema = mongoose.Schema({
   }
 })
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({email})
+
+
+  if (!user) {
+    throw new Error('Unable to login')
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch) {
+    throw new Error('Unable to login')
+  }
+
+  return user
+}
+
 userSchema.pre('save', async function (next) {
-  console.log('just before saving');
   if(this.isModified('password')) {
     this.password = await bcrypt.hash(this.password, 8)
   }
--- src/routers/user.js
+++ src/routers/user.js
@@ -8,8 +8,18 @@ router.post('/users', async (req, res) => {
     await user.save()
     res.status(201).send(user)
   } catch (e) {
-    console.log(e);
     res.status(400).send(e)
+  }
+})
+
+router.post('/users/login', async (req, res) => {
+  const {email, password} = req.body
+
+  try {
+    const user = await User.findByCredentials(email, password)
+    res.send(user)
+  } catch (e) {
+    res.status(400).send()
   }
 })
 
