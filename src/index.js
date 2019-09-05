d7f99456490f7560f1ab0a69751eefdd3216e77c
max-powell <github email address>
2019-09-05 11:43:10 +0100

    moved user routes into separate file

--- src/index.js
+++ src/index.js
@@ -1,88 +1,15 @@
 const express = require('express')
 require('./db/mongoose')
 
-const User = require('./models/User')
 const Task = require('./models/Task')
+
+const userRouter = require('./routers/user')
 
 const app = express ()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-app.post('/users', async (req, res) => {
-  const user = new User(req.body)
-  try {
-    await user.save()
-    res.status(201).send(user)
-  } catch (e) {
-    res.status(400).send(e)
-  }
-})
-
-app.get('/users', async (req, res) => {
-  try {
-    const users = await User.find({})
-    res.send(users)
-  } catch (e) {
-    res.status(500).send()
-  }
-})
-
-app.get('/users/:id', async (req, res) => {
-  const _id = req.params.id
-
-  try {
-    const user = await User.findById(_id)
-    if (!user) {
-      return res.status(404).send()
-    }
-    res.send(user)
-  } catch (e) {
-    res.status(404).send(e)
-  }
-})
-
-app.patch('/users/:id', async (req, res) => {
-  const _id = req.params.id
-  const updates = req.body
-  const allowedUpdates = Object.keys(User.schema.obj)
-
-  const isValid = Object.keys(updates).every(u => allowedUpdates.includes(u))
-
-  if (!isValid) {
-    res.status(400).send({error: 'Invalid fields'})
-  }
-
-  try {
-
-    const user = await User.findByIdAndUpdate(_id, updates, {
-      new: true,
-      runValidators: true
-    })
-
-    if (!user) {
-      return res.status(404).send()
-    }
-
-    res.send(user)
-  } catch (e) {
-    res.status(400).send(e)
-  }
-})
-
-app.delete('/users/:id', async (req, res) => {
-  const _id = req.params.id
-
-  try {
-    const user = await User.findByIdAndDelete(_id)
-    if (!user) {
-      return res.status(404).send()
-    }
-    res.send(user)
-  } catch (e) {
-    res.status(500).send()
-  }
-})
+app.use(userRouter)
 
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
