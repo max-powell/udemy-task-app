d09046a6c9b4ab570185565f8e48313d0f420e09
max-powell <github email address>
2019-09-05 12:53:54 +0100

    refactored task update route to use middleware

--- src/routers/task.js
+++ src/routers/task.js
@@ -48,10 +48,13 @@ router.patch('/tasks/:id', async (req, res) => {
   }
 
   try {
-    const task = await Task.findByIdAndUpdate(_id, updates, {
-      new: true,
-      runValidators: true
-    })
+    const task = await Task.findById(_id)
+
+    for (u in updates) {
+      task[u] = updates[u]
+    }
+
+    await task.save()
 
     if (!task) {
       return res.status(404).send()
