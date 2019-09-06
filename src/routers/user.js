ac8ae32fde859e8e41428ae29c3893aa15330dfe
max-powell <github email address>
2019-09-06 15:58:17 +0100

    added upload avatar route

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const multer = require('multer')
 const User = require('../models/user')
 const router = new express.Router()
 const auth = require('../middleware/auth')
@@ -89,6 +90,23 @@ router.delete('/users/me', auth, async (req, res) => {
   } catch (e) {
     res.status(500).send()
   }
+})
+
+const upload = multer({
+  dest: 'avatars',
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload an image'))
+    }
+    cb(undefined, true)
+  }
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+  res.send()
 })
 
 module.exports = router
