2682f047a898d6635cc8ae105003ff3498265d25
max-powell <github email address>
2019-09-06 10:01:06 +0100

    deleted get user by id route

--- src/routers/user.js
+++ src/routers/user.js
@@ -52,20 +52,6 @@ router.get('/users/me', auth, async (req, res) => {
   res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
-  const _id = req.params.id
-
-  try {
-    const user = await User.findById(_id)
-    if (!user) {
-      return res.status(404).send()
-    }
-    res.send(user)
-  } catch (e) {
-    res.status(404).send(e)
-  }
-})
-
 router.patch('/users/:id', async (req, res) => {
   const _id = req.params.id
   const updates = req.body
